98998010627a8824cc2c0fb293fd2a32e95eb645
hsgyeong <sgyeong0711@aiicon.kr>
2024-06-08 23:59:19 +0900

    css

---
+++ javascript/src/main/webapp/test.js
@@ -0,0 +1,9 @@
+
+var target = document.querySelector(".box2");
+var btn = document.querySelector("button");
+
+btn.addEventListener("click", function(){
+	var pre = parseInt(target.style.left);
+	target.style.left = pre + 100 + "px";
+});
+
