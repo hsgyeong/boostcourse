7cdf6508dc9bf228b751931c14940feed3a8aa94
sgyeong <sgyeong0711@gmail.com>
2024-06-09 21:35:52 +0900

    디렉토리 구성

--- javascript/src/main/webapp/test.js
+++ javascript/src/main/webapp/test.js
@@ -1,9 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+	var target = document.querySelector(".box2");
+	var btn = document.querySelector("button");
 
-var target = document.querySelector(".box2");
-var btn = document.querySelector("button");
-
-btn.addEventListener("click", function(){
-	var pre = parseInt(target.style.left);
-	target.style.left = pre + 100 + "px";
+	btn.addEventListener("click", function(){
+		var pre = parseInt(target.style.left);
+		target.style.left = pre + 200 + "px";
+	
+	})
 });
 
