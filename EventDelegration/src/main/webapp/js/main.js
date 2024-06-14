7736fd43462d484fd519f507052a5087cfa1ccaf
hsgyeong <sgyeong0711@gmail.com>
2024-06-14 23:51:36 +0900

    event delegation

--- EventDelegration/src/main/webapp/js/main.js
+++ EventDelegration/src/main/webapp/js/main.js
@@ -8,12 +8,23 @@ img.addEventListener("click", function(){
 })
 
 ul.addEventListener("click", function(evt){
+	
+	var target = evt.target;
+	if(target.tagName === "IMG" ){
+		log.innerHTML = "IMG URL은," + evt.target.src;
+	} else if (evt.target.tagName === "LI"){
+		log.innerHTML = "IMG URL은, " + evt.target.firstElementChild.src;
+	}
+});
+
+ul.addEventListener("click", function(evt){
 	console.log(evt.currentTarget.tagName, evt.target.tagName);
 });
 	
-/*for(var i = 0, len=lists.length; i<lists.length; i++){
+for(var i = 0, len=lists.length; i<lists.length; i++){
 	lists[i].addEventListener("click", function(evt){
 		console.log(evt.currentTarget);
 		log.innerHTML = "clicked" + evt.currentTarget.firstElementChild.src;
 	});
-}*/
+}
+
